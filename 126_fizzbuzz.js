d5cae0dc8001f51db6f542ea22c705d176c79ad2
Mark Klocek <Mark.Klocek@GMail.com>
2025-06-07 13:32:52 -0700

    P.R.E.P practice

---
+++ 126_fizzbuzz.js
@@ -0,0 +1,17 @@
+//PREP 
+//1 to num, 3 fizz, 5 buzz, 3&5 fizzbuzz
+//Will this always be a number? Will it always be whole? will it be negative? Are there any other gotchas t hat might come up here?
+//P(arameters): num, whole, always positive
+//R(eturn): console
+//E(xample):
+
+function fizzBuzz(num){
+    //loop
+    //conditional %3&%5, %3, %5, 
+    //console.log num fizz buzz or fizzbuzz
+
+}
+
+fizzBuzz(5)//1,2,fizz,4,buzz
+fizzBuzz(3)//1,2,fizz
+fizzBuzz(15)//1,2,fizz,4,buzz,fizz,7,8,fizz,buzz,11,fizz,13,14,fizzbuzz
