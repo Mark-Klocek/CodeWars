1726a621cd262529778fe58894f7c5239ded0f1d
Mark Klocek <Mark.Klocek@GMail.com>
2025-06-14 18:13:39 -0700

    misc

---
+++ 136_the_vowel_code.js
@@ -0,0 +1,36 @@
+// Step 1: Create a function called encode() to replace all the lowercase vowels in a given string with numbers according to the following pattern:
+
+// a -> 1
+// e -> 2
+// i -> 3
+// o -> 4
+// u -> 5
+
+// For example, encode("hello") would return "h2ll4". There is no need to worry about uppercase vowels in this kata.
+
+// Step 2: Now create a function called decode() to turn the numbers back into vowels according to the same pattern shown above.
+
+// For example, decode("h3 th2r2") would return "hi there".
+
+// For the sake of simplicity, you can assume that any numbers passed into the function will correspond to vowels.
+
+
+
+//E:
+// let string = "Programming is fun!"
+// let string = "Decode this sentence now."
+// let string = "AEIOU are vowels."
+// let string = "Why did you leave me?"
+// let string = "Encryption complete."
+// let string = "hello world"
+// let string = "Can you read this?"
+// let string = "Testing one two three."
+// let string = "This is a simple string."
+// let string = "Vowels will be encoded."
+function encode(string) {
+  return
+}
+
+function decode(string) {
+  return
+}
