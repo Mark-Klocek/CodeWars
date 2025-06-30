cc59edfd575d8152361df43692e8d4d66b50b79f
Mark Klocek <Mark.Klocek@GMail.com>
2025-06-30 13:54:47 -0700

    code wars 144 solution

--- 144_find_all_occurances.js
+++ 144_find_all_occurances.js
@@ -8,7 +8,29 @@
 
 // findAll([6, 9, 3, 4, 3, 82, 11], 3) => [2, 4]
 
+//P: Given an array and a number n - can it be an array of numbers, strings, functions, what is in the  array? will the array always have something in it ? will it ever not be an array?
+
+//R: We are returning an array of index numbers where n is located in the passed array
+
+
+
 
 function findAll(array, n) {
-  // your code here
+  returnArray = []
+  array.forEach((element,index)=>{
+    if (element === n) returnArray.push(index)
+  })
+  return returnArray
 }
+
+
+//E:
+console.log(JSON.stringify(findAll([1, 2, 3, 2, 1, 2, 5], 2)) == JSON.stringify([1, 3, 5]));
+console.log(JSON.stringify(findAll([5, 5, 5, 5, 5], 5)) == JSON.stringify([0, 1, 2, 3, 4]));
+console.log(JSON.stringify(findAll([0, 1, 2, 3, 4, 5], 6)) == JSON.stringify([]));
+console.log(JSON.stringify(findAll([9, 8, 7, 6, 5, 4, 3], 4)) == JSON.stringify([5]));
+console.log(JSON.stringify(findAll([10, 20, 30, 20, 10], 10)) == JSON.stringify([0, 4]));
+console.log(JSON.stringify(findAll([], 1)) == JSON.stringify([]));
+console.log(JSON.stringify(findAll([100, 200, 300], 200)) == JSON.stringify([1]));
+console.log(JSON.stringify(findAll([7, 7, 8, 9, 7], 7)) == JSON.stringify([0, 1, 4]));
+console.log(JSON.stringify(findAll([1, 3, 5, 7], 2)) == JSON.stringify([]));
