0cadfe17f0a330293b29e4146ffe8bab6955f07d
Mark Klocek <Mark.Klocek@GMail.com>
2025-07-01 14:13:09 -0700

    code wars 145 grid question

---
+++ 145_training_on_grid_path_x.js
@@ -0,0 +1,23 @@
+// Given an n by n grid filled with lowercase x's and periods (.), return the minimum number of periods to be replaced with x's in order to form a continuous, straight, n-length path of x's, from either: side to side, top to bottom, or diagonally.
+// Input and Output:
+
+//     Input: a string formatted as an n by n grid filled with randomly placed lowercase x's and periods. Each line of the grid is separated from the next by a linebreak.
+
+//     Output: a number between 0 and n, inclusive. The return number represents the minimum number of periods to be replaced with x's to obtain a straight n-length path of x's.
+
+// Example:
+
+//     Input:
+
+//   ..x..
+//   .....
+//   .....
+//   ..x..
+//   xx..x 
+
+//     Output: 2
+
+
+function minXPath(grid) {
+
+}
