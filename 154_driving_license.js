43525d161287d170ef063a1e9d9977daf530a2b5
Mark Klocek <Mark.Klocek@GMail.com>
2025-07-10 14:38:35 -0700

    codewars 154 solution

--- 154_driving_license.js
+++ 154_driving_license.js
@@ -25,6 +25,21 @@
 // 2 = Surname
 // 3 = Date of Birth (In the format Day Month Year, this could include the full Month name or just shorthand ie September or Sep)
 // 4 = M-Male or F-Female
+
+
+
+
 function driver(data) {
-  // Code here
+  console.log(data)
 }
+
+console.log(JSON.stringify(driver(["John","James","Smith","01-Jan-2000","M"])) === JSON.stringify("SMITH900101JJ9AA"));
+console.log(JSON.stringify(driver(["Sarah","","O'Neil","23-Jul-1984","F"])) === JSON.stringify("ONEIL884237S99AA"));
+console.log(JSON.stringify(driver(["Peter","","Lee","05-Mar-1979","M"])) === JSON.stringify("LEE9990305P99AA"));
+console.log(JSON.stringify(driver(["Amanda","Grace","Wong","17-Sep-1993","F"])) === JSON.stringify("WONGG993175A99AA"));
+console.log(JSON.stringify(driver(["David","Andrew","Ng","30-Dec-1962","M"])) === JSON.stringify("NG99996230DA9AA"));
+console.log(JSON.stringify(driver(["Emily","","Jones","12-May-1988","F"])) === JSON.stringify("JONES885212E99AA"));
+console.log(JSON.stringify(driver(["George","Michael","Thompson","01-Jan-2001","M"])) === JSON.stringify("THOMP901101GM9AA"));
+console.log(JSON.stringify(driver(["Linda","","Brown","22-Nov-1970","F"])) === JSON.stringify("BROWN972227L99AA"));
+console.log(JSON.stringify(driver(["James","Lee","Chan","05-Feb-1999","M"])) === JSON.stringify("CHAN9990205JL9AA"));
+console.log(JSON.stringify(driver(["Nina","","Kaur","19-Aug-1985","F"])) === JSON.stringify("KAUR998195N99AA"));
