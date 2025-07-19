5ff70bb6805309a0a0c1ef24f95d79825a19fb77
Mark Klocek <Mark.Klocek@GMail.com>
2025-07-18 19:58:16 -0700

    codewars 158

---
+++ 157_js_golfing_6_string_summation.js
@@ -0,0 +1,17 @@
+// Task:
+
+// Return the summation of the input strings with no more than 14 characters.
+//P: We will be given two numbers as strings
+//R: returned the sum of the two strings
+//e: '21','2' = 24
+// '14','5' = 19
+
+function x(a,b){
+//P
+//transform a into a number
+//transform b into a number
+//return the sum of a and b
+    if (a.length < 14){
+        return Number(a) + Number(b)
+    }
+}
