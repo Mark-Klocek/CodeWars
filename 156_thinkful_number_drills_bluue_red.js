2d33c9899bdb6739f24bbb858ed6977ca88ee972
Mark Klocek <Mark.Klocek@GMail.com>
2025-07-21 17:00:20 -0700

    code wars 159 problem

--- 156_thinkful_number_drills_bluue_red.js
+++ 156_thinkful_number_drills_bluue_red.js
@@ -29,9 +29,9 @@ function guessBlue(blueStart, redStart, bluePulled, redPulled) {
   //divide totalBlue by the sum of totalBlue and totalRed
   //return that result
 
-  let blueTotal = 
-  let redTotal = redStart - redPulled
-  return blueStart - bluePulled/ (blueStart - bluePulled + redStart - redPulled)
+ // let blueTotal = 
+  //let redTotal = redStart - redPulled
+ // return blueStart - bluePulled/ (blueStart - bluePulled + redStart - redPulled)
 
     
 }
