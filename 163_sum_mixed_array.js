8f7df2d1c1c3f39540f922a5fe127786f00beb54
Mark Klocek <Mark.Klocek@GMail.com>
2025-07-25 08:08:26 -0700

    code wars 163 solution

--- 163_sum_mixed_array.js
+++ 163_sum_mixed_array.js
@@ -3,9 +3,14 @@
 // Return your answer as a number.
 //P: We will be given an array of integers, will they be positive? will they sometimes be negative? they will always either be a string or int version of a number? no actual string words?
 
-//R: We want to return 
+//R: We want to return a number of the sum of the array
+
+//E: [4,3,'1','9'] => 17
 
 
 function sumMix(x){
-
+    //P: use reduce
+    //during reduce, add + to element to convert it to number
+    //return total number
+    return x.reduce((acc,c)=>acc += +c,0)
 }
