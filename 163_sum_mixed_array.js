0e7157af3146972e52b94c400e7bfafb8a53cb76
Mark Klocek <Mark.Klocek@GMail.com>
2025-07-25 08:06:15 -0700

    code wars 163 exercise

---
+++ 163_sum_mixed_array.js
@@ -0,0 +1,11 @@
+// Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
+
+// Return your answer as a number.
+//P: We will be given an array of integers, will they be positive? will they sometimes be negative? they will always either be a string or int version of a number? no actual string words?
+
+//R: We want to return 
+
+
+function sumMix(x){
+
+}
