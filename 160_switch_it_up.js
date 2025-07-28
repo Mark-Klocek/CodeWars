63bb22acbc2fc5123dea762493619067985b787f
Mark Klocek <Mark.Klocek@GMail.com>
2025-07-28 09:20:59 -0700

    code wars switch it up solution

--- 160_switch_it_up.js
+++ 160_switch_it_up.js
@@ -7,5 +7,42 @@
 // If your language supports it, try using a switch statement.
 function switchItUp(number){
 //Write your own Code!
-    return String(number)
+    let num = null
+    switch(number){
+        case 0:
+            num = 'Zero'
+            break;
+        case 1:
+            num = 'One'
+            break;
+        case 2:
+            num = 'Two'
+            break;
+        case 3:
+            num = 'Three'
+            break;
+        case 4:
+            num = 'Four'
+            break;
+        case 5:
+            num = 'Five'
+            break;
+        case 6:
+            num = 'Six'
+            break;
+        case 7:
+            num = 'Seven'
+            break;
+        case 8:
+            num = 'Eight'
+            break;
+        case 9:
+            num = 'Nine'
+            break;
+        
+        default:
+            num = 'ERROR'
+            break;
+    }
+    return num
 }
