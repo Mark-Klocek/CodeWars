6d3f5f4688016726d28a06808333b62557593026
Mark Klocek <Mark.Klocek@GMail.com>
2025-08-10 12:22:15 -0700

    code wars 177 solution

--- 177_arrh_grabscrab.js
+++ 177_arrh_grabscrab.js
@@ -14,7 +14,26 @@
 
 // Good luck!
 
+//Parameters We are given a string of letters, and an array of words
 
+//Return we are returning an array of words that the letters might have meant. The array of words must be in the same order that htey occured in the original array
+//      if there are no matches, we are returning an empty array
+
+//E: "ortsp", ["sport", "parrot", "ports", "matey"] => ["sport", "ports"]
 function grabscrab(anagram, dictionary) {
+    //Psuedo code
+    //We want to create a returnArray and initialize it as an empty array
+    //we want to re-assign the anagram to the sorted version of itself
+    //take the dicitonary array, iterate through each. compare each sorted version of the element to the anagram, and if they are the same, push the element to the returnArray
+    //return the returnArray
+    let returnArray = []
+    anagram = (anagram.split('').sort().join(''))
+    dictionary.forEach(element=>{
+        sortedElement = element.split('').sort().join('')
+        sortedElement === anagram ? returnArray.push(element) : ''
+    })
+    return returnArray
   // Your code here
 }
+
+console.log(grabscrab('trisf',['first']))
