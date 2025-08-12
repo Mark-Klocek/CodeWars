160b21097a91e5c5a5a867c4659509a017dd6369
Mark Klocek <Mark.Klocek@GMail.com>
2025-08-12 09:42:57 -0700

    codewars 179 exercise

---
+++ 179_find_added.js
@@ -0,0 +1,25 @@
+// You are given two strings (st1, st2) as inputs. Your task is to return a string containing the numbers in st2 which are not in str1. Make sure the numbers are returned in ascending order. All inputs will be a string of numbers.
+console.log('test')
+// Here are some examples:
+
+// findAdded('4455446', '447555446666'); // '56667'
+// findAdded('44554466', '447554466'); // '7'
+// findAdded('9876521', '9876543211'); // '134'
+// findAdded('678', '876'); // ''
+// findAdded('678', '6'); // ''
+
+//P We are given two strings of numbers
+
+//R: we ar returning st2 numbers which aren't in str1
+
+//E: str1 = '4455446' str2 = '447555446666' => '56667'
+
+let str1 = '4455446'
+let str2 = '447555446666'
+function findAdded(st1, st2){
+    
+    return 
+}
+
+
+console.log(findAdded(str1,str2))
