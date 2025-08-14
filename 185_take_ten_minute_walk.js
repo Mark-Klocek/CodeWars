736ddfbc235de8a7c31e9dc3c827bb97bd3b4166
Mark Klocek <Mark.Klocek@GMail.com>
2025-08-14 12:41:02 -0700

    codewars 185 actual solution, prior was exercise

--- 185_take_ten_minute_walk.js
+++ 185_take_ten_minute_walk.js
@@ -2,6 +2,70 @@
 
 //     Note: you will always receive a valid array containing a random assortment of direction letters ('n', 's', 'e', or 'w' only). It will never give you an empty array (that's not a walk, that's standing still!).
 
+//P We are given an array of characters representing directions that one will walk. Will it always be nsew? wil it ever have capital letters? do we have to verify if there are correct inputs?
+
+//R: we are returning a boolean on whether or not the directions would BOTH last only 10 minutes, and return the person back to its starting point
+
+//E: ['n','s'] => false, is not 10 minutes long
+
+
 function isValidWalk(walk) {
-  //insert brilliant code here
+    //P Verify if the walk is 10 length, if not, return false
+    //instantiate 4 directions at 0 - north, south, east, west. Since we are starting at 0,0, all directions are 0 away from the starting point.
+    //check for each input - we check to see if the opposite is positive, if it is, we remove one from the opposite direction, if not, we add one to current direction
+    //return north+south+east+west == 0
+
+    
+    //verifying its a valid length route
+    if (walk.length !== 10){
+        return false
+    }
+
+    let north = 0
+    let south = 0
+    let east = 0
+    let west = 0
+
+    walk.forEach(element=>{
+        switch(element){
+            case 'n':
+                if (south===0){
+                    north ++
+                }else{
+                    south--
+                }
+                break;
+            case 's':
+                if (north ===0){
+                    south++
+                }else{
+                    north--
+                }
+                break;
+            case 'e':
+                if (west ===0){
+                    east ++
+                }else{
+                    west --
+                }
+                break;
+            case 'w':
+                if(east ===0){
+                    west++
+                }else{
+                    east--
+                }
+                break;
+            default:
+                break;
+        }
+    })
+
+    return (north+south+east+west === 0)
+
+
+
+
+
 }
+console.log(isValidWalk(['w','e','w','e','w','e','w','e','w','e','w','e']))
