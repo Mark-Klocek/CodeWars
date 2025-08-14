a2f1abbb74df2f11226ff3db935c85b351f7461f
Mark Klocek <Mark.Klocek@GMail.com>
2025-08-14 13:15:31 -0700

    code wars 186 solution

--- 186_biggest_search_key.js
+++ 186_biggest_search_key.js
@@ -17,6 +17,35 @@
 
 //E: ['one','two','three','seven'] => ['seven','three']
 
-function theBiggestSearchKeys() {
-  //your code here
+function theBiggestSearchKeys(...args) {
+    
+  //P:instantiate an empty array
+
+  //sort given arguments based on length descending
+
+  //starting left side, push that item into the empty array because we know it's the longest.
+
+  //if the empty array is only length 1, return 0 position of new array
+
+  //if empty array length > 1, return that array sorted
+  longestKey = 0
+  if (args.length === 0){
+    return '\'\''
+  }
+  returnArray = []
+    args = args.sort((a,b)=> b.length - a.length)
+    for (let i = 0;i<args.length;i++){
+        if (i === 0){
+            longestKey = args[i].length
+            returnArray.push(`\'${args[i]}\'`)
+        }else{
+            if (args[i].length === longestKey){
+                returnArray.push(`\'${args[i]}\'`)
+            }else{
+                break
+            }
+        }
+    }
+    return returnArray.length > 1 ? returnArray.sort().join(', '):returnArray[0]
 }
+console.log(theBiggestSearchKeys("key1", "key22", "key333"),)
