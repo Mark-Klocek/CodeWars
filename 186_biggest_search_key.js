8bebd49f2cabb6a2c8498fe27f03c24822edd8a5
Mark Klocek <Mark.Klocek@GMail.com>
2025-08-14 12:54:03 -0700

    code wars 186 exercise

---
+++ 186_biggest_search_key.js
@@ -0,0 +1,22 @@
+// You're a programmer in a SEO company. The SEO specialist of your company gets the list of all project keywords everyday, then he looks for the longest keys to analyze them.
+
+// You will get the list with keywords and must write a simple function that returns the biggest search keywords and sorts them in lexicographical order.
+
+// For instance you might get:
+
+// 'key1', 'key2', 'key3', 'key n', 'bigkey2', 'bigkey1'
+
+// And your function should return:
+
+// "'bigkey1', 'bigkey2'"
+
+
+//P: we are given a function that has several arguments pased into it
+
+//R: we are returning the longest argument, and if there are multiple with the same length, they want the array returned alphabeticaly
+
+//E: ['one','two','three','seven'] => ['seven','three']
+
+function theBiggestSearchKeys() {
+  //your code here
+}
