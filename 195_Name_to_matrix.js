ec04c9faae360bff8e77ca7beaa0c1487fd51ae0
Mark Klocek <Mark.Klocek@GMail.com>
2025-09-02 09:56:35 -0700

    code wars 195 solution

--- 195_Name_to_matrix.js
+++ 195_Name_to_matrix.js
@@ -20,7 +20,43 @@
 
 
 function matrixfy(str) {
+    //psuedo
+    //first thing we want to do is determine how large the return array is going to be
+    //we can determine this, by taking the square root of the length of the name, applying ceiling to it, then squaring that number
+    //we can iterate through this square matrix by first, iterating through the initial ceiling, then within each of those iterations, iterate again through that cieling, effectively creating a perfect matrix
+    //we take the original string, and while iterating, shift the letter off of the string. do an if check to see if the string is empty, if it is, instead of doing the shift, we just set it to a period
+    if(str.length < 1){
+        return "name must be at least one letter"
+    }
+    //setting matrixSize as the ceilling of the sqrt of the length of the string
+    let matrixSize = Math.ceil(Math.sqrt(str.length))
+    let returnArray = []
+    //making str into an array so i can do a length check later in the array
+    str = str.split('')
+
+    //iterating creating inner arrays tempArray
+    for(let i = 0;i<matrixSize;i++){
+        tempArray = []
+        //within each innerarray
+        for(let j = 0; j<matrixSize;j++){
+            //if the str array(which is hte name) is not empty, we shift the first letter off and push it onto the inner array
+            if(str.length > 0){
+                let c = str.shift()
+                tempArray.push(c)
+            }else{
+                //else if the string array (name) is now empty, we can push a "."onto the temp array
+                tempArray.push('.')    
+            }
+            
+        }
+        //pushing the created temp array onto the returnArray
+        returnArray.push(tempArray)
+    }
     
+    
+    
+    return returnArray
+
     
 }
 
