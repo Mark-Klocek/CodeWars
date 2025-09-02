f12ddfd0b54300a0fbfd1ef07b1c13ba7b467b79
Mark Klocek <Mark.Klocek@GMail.com>
2025-09-02 09:56:25 -0700

    code wars 195 exercise

---
+++ 195_Name_to_matrix.js
@@ -0,0 +1,27 @@
+// Given a name, turn that name into a perfect square matrix (nested array with the amount of arrays equivalent to the length of each array).
+
+// You will need to add periods (.) to the end of the name if necessary, to turn it into a matrix.
+
+// If the name has a length of 0, return "name must be at least one letter"
+// Examples
+
+// "Bill" ==> [ ["B", "i"],
+//              ["l", "l"] ]
+
+// "Frank" ==> [ ["F", "r", "a"],
+//               ["n", "k", "."],
+//               [".", ".", "."] ]
+
+//Params: We are given a string that is a name, will it ever be empty? will it always be a string? Do we ever have to convert it>
+
+//Return: we are to turn that string into a perfect matrix of arrays. If the matrix is larger than the letters of hte name, we are to replace each element in the matrix of arrays with a "."
+
+//Example: "Greg" => [[G,r],[e,g]]
+
+
+function matrixfy(str) {
+    
+    
+}
+
+console.log(matrixfy("wow"))
