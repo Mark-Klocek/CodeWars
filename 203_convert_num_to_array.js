4baa246d5edfaf54d9706ed05333aa10d81b2452
Mark Klocek <Mark.Klocek@GMail.com>
2025-09-11 18:33:02 -0700

    code wars 203 solution

--- 203_convert_num_to_array.js
+++ 203_convert_num_to_array.js
@@ -1,9 +1,17 @@
 // Given a random non-negative number, you have to return the digits of this number within an array in reverse order.
 // Example (Input => Output):
 
-// 35231 => [1,3,2,5,3]
+// 35231
 // 0     => [0]
 
 function digitize(n) {
-  //code here
+  
+  
+  let stringArray = String(n).split('').reverse()
+  return stringArray.map(element => +element)
+  
+  
+  
+  
+  
 }
