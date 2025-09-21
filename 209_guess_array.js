1d1c55eabc71b8db9c14005219514f155facc112
Mark Klocek <Mark.Klocek@GMail.com>
2025-09-20 19:12:47 -0700

    code wars 209 solution

--- 209_guess_array.js
+++ 209_guess_array.js
@@ -16,6 +16,22 @@
 
 // The array will always have at least 3 elements.
 
-function guess(f, i) {
-    return [];
+function guess(f, n) {
+  let res = new Array(n);
+
+  // Get first three
+  let a01 = f(0, 1);
+  let a12 = f(1, 2);
+  let a02 = f(0, 2);
+
+  res[0] = (a01 + a02 - a12) / 2;
+  res[1] = a01 - res[0];
+  res[2] = a02 - res[0];
+
+  // Remaining
+  for (let i = 3; i < n; i++) {
+    res[i] = f(i - 1, i) - res[i - 1];
+  }
+
+  return res;
 }
