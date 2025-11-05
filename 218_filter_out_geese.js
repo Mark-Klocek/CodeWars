f68db1f699cb84cc79bcb1fd6629fe1cc27d5ce1
Mark Klocek <Mark.Klocek@GMail.com>
2025-11-05 13:54:05 -0800

    codewars 218 solution

--- 218_filter_out_geese.js
+++ 218_filter_out_geese.js
@@ -1,5 +1,23 @@
+//P - we are given an array of strings, each string will be a word. Will there only ever be strings in this array? Does it matter if theres a number and we return the number in the string? 
+
+//R We return an array of strings filtering out words that exist in the geese array
+
+//E: gooseFilter(['blah','weep','red','African']) => ['blah','weep','red']
+
+
+
+
+
+
+
 function gooseFilter (birds) {
   var geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"];
   
-  // return an array containing all of the strings in the input array except those that match strings in geese
+  //P
+  //return filtered version of birds
+  //each filter iteration checks to see if current element exists in birds
+  //if it doesnt exist, it passes through the filter
+  //returns the filtered birds not containing the goose species
+
+  return birds.filter((element)=> !geese.includes(element))
 };
