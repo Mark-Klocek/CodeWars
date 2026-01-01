b3637f6e366bdacc99a3496806dafa8af5d8c208
Mark Klocek <Mark.Klocek@GMail.com>
2026-01-01 15:42:20 -0800

    codewars 223 soltuion

--- 223_rock_paper_scissors.js
+++ 223_rock_paper_scissors.js
@@ -8,5 +8,9 @@
 // Let's play! You will be given valid moves of two Rock, Paper, Scissors players, and have to return which player won: "Player 1 won!" for player 1, and "Player 2 won!" for player 2. In case of a draw return Draw!.
 
 const rps = (p1, p2) => {
-   
+    if (p1 == p2) return "Draw!"
+    else if(p1 == 'scissors' && p2 == 'paper') return "Player 1 won!"
+    else if(p1 == 'paper' && p2 == 'rock') return "Player 1 won!"
+    else if(p1 == 'rock' && p2 == 'scissors') return "Player 1 won!"
+    else return "Player 2 won!"
 };
