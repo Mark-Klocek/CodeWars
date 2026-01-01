738c56a1f2f0d39a16e7d152672fa070be08781e
Mark Klocek <Mark.Klocek@GMail.com>
2026-01-01 15:42:07 -0800

    code wars 223 exercise

---
+++ 223_rock_paper_scissors.js
@@ -0,0 +1,12 @@
+// Rules of the "Rock, Paper, Scissors" game are:
+
+//     Rock beats Scissors,
+//     Scissors beat Paper,
+//     Paper beats Rock,
+//     Two identical moves are a draw.
+
+// Let's play! You will be given valid moves of two Rock, Paper, Scissors players, and have to return which player won: "Player 1 won!" for player 1, and "Player 2 won!" for player 2. In case of a draw return Draw!.
+
+const rps = (p1, p2) => {
+   
+};
