148157f9c197b51489d81955ab5bc7144b428e12
Mark Klocek <Mark.Klocek@GMail.com>
2026-01-06 16:43:53 -0800

    codewars 255 solution

--- 225_divisible_by.js
+++ 225_divisible_by.js
@@ -1,3 +1,5 @@
+console.log('divisible_by')
+
 // Create a function that checks if the first argument n is divisible by all other arguments (return true if no other arguments)
 
 // Example:
@@ -7,6 +9,15 @@
 // (100,5,4,10,25,20)--> true
 // (12,7)--> false because 12 is not divisible by 7
 
-function isDivisible(){
+function isDivisible(...n){
+    let isDiv = true
+    n.forEach(e=> {
+        if (n[0] % e !== 0){
+            isDiv = false
+        }
+    })
+    return isDiv
   //Write your code here
 }
+
+console.log(isDivisible(13,4))
