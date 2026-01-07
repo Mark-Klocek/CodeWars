2e45786fc876f55e09ba4dde3b5930c2f85cdd8b
Mark Klocek <Mark.Klocek@GMail.com>
2026-01-06 16:57:52 -0800

    codewars 226 solution

--- 226_broken_collatz.js
+++ 226_broken_collatz.js
@@ -10,10 +10,10 @@ console.log('226, broken collatz')
 function collatz(n, count = 1) {
 
 
-    
+
     if (n == 1) return count;
-    n % 2 == 0 ? n = n / 2 : n = n * 3 + 1;
-    collatz(n, count + 1);
+    n = n % 2 == 0 ? n / 2 : n * 3 + 1;
+    return collatz(n, count + 1);
 }
 
 console.log(collatz(2))
