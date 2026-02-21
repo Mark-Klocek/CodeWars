f5ce2b5278d2a4dd1cb4455d9e2f6196578e7d3c
Mark Klocek <Mark.Klocek@GMail.com>
2026-02-20 16:35:08 -0800

    codewars 231 solution

--- 231_freudian.js
+++ 231_freudian.js
@@ -5,8 +5,12 @@
 // Now, basically, when a person hears about Freud, s/he hears "sex" because for Freud, everything was related to, and explained by sex.
 
 // In this kata, the function will take a string as its argument, and return a string with every word replaced by the explanation to everything, according to Freud. Note that an empty string, or no arguments, should return an empty string.
-
+console.log("CW 231")
+let string = "This is a longer test"
 
 function toFreud(string) {
-  return string;
+    return string.length > 0 ? string.split(" ").map((element,index)=> element = "sex").join(" "): ""
+  
 }
+
+console.log(toFreud(string))
