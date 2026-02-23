5f91da2893b839308729d8503598c34caae7b307
Mark Klocek <Mark.Klocek@GMail.com>
2026-02-23 13:42:40 -0800

    codewars 237 exercise

---
+++ 237_remove_marked.js
@@ -0,0 +1,10 @@
+// Define a method/function that removes from a given array of integers all the values contained in a second array.
+
+let testArray = [1, 1, 2, 3, 1, 2, 3, 4, 4, 3, 5, 6, 7, 2, 8]
+let testMarked = [1, 3, 4, 2]
+
+function removedMark(parentArray,markedArray){
+    
+}
+
+console.log(removedMark(testArray, testMarked))
