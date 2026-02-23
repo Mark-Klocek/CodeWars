320be1504191ae94b283dbb67f02a65b48d3768c
Mark Klocek <Mark.Klocek@GMail.com>
2026-02-23 14:45:51 -0800

    codewars 240 solution

--- 240_reverse_sequence.js
+++ 240_reverse_sequence.js
@@ -2,6 +2,14 @@
 
 // Example : n=5 --> [5,4,3,2,1]
 
+let n = 5
 const reverseSeq = n => {
-  return [];
+    let returnArr = []
+    for(let i = 1;i <= n; i++){
+        returnArr.push(i)
+    }
+    return returnArr.reverse()
+  
 };
+
+console.log(reverseSeq(n))
