fdbbe5d47fd7500f6063c59946d1c7185199d20f
Mark Klocek <Mark.Klocek@GMail.com>
2026-02-23 13:27:49 -0800

    codewars 236 exercise

---
+++ 236_bin_to_dec.js
@@ -0,0 +1,10 @@
+// Complete the function which converts a binary number (given as a string) to a decimal number.
+
+
+let testers = '1001001'
+function binToDec(bin) {
+  
+}
+
+
+console.log(binToDec(testers))
