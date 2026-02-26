421a430f60904cdf5ae0fea64df5303603042fbc
Mark Klocek <Mark.Klocek@GMail.com>
2026-02-26 13:56:20 -0800

    codewarsa 242 solution

--- 242_real_floor.js
+++ 242_real_floor.js
@@ -9,5 +9,11 @@
 // Basements (negatives) stay the same as the universal level.
 
 function getRealFloor(n) {
-    
+    if (n<=0){
+        return n
+    }else if (n <= 13){
+        return n-1
+    }else{
+        return n-2
+    }
 }
