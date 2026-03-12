e11ce69c34a66635c5b2badc85b2f07fd6c0f453
Mark Klocek <Mark.Klocek@GMail.com>
2026-03-12 10:40:22 -0700

    codewars 263 solution

--- 263_sequence_to_1.js
+++ 263_sequence_to_1.js
@@ -10,7 +10,19 @@
 // Ruby       -9999 < n < 9999
 
 function seqToOne(n){
-  
+  let returnArray = []
+
+  if (n < 1){
+    for(let i = n;i<1;i++){
+        returnArray.push(i)
+    }
+  }else if (n > 1){
+    for(let i  = n;i>1;i--){
+        returnArray.push(i)
+    }
+  }
+  returnArray.push(1)
+  return returnArray
 }
 
 console.log(seqToOne(10))
