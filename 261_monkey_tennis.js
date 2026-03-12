539526806928fe8e6b5f2d1b0ff66839c23b0a1c
Mark Klocek <Mark.Klocek@GMail.com>
2026-03-12 09:45:02 -0700

    codewars 261 solution

--- 261_monkey_tennis.js
+++ 261_monkey_tennis.js
@@ -7,7 +7,15 @@
 // Your job is to gather up just the tennis balls and return them in a sack. The sack will be represented by an object that has a 'weight' property, the value of which should be equal to the combined weight of all tennis balls in the array. If the array contains no tennis balls, then the weight value of the returned sack object should be 0.
 
 function ballCollector(detritus) {
-  
+  let tennisBag = {}
+    let tennisBallWeight = 0
+  detritus.forEach((element)=>{
+    if (element == 58){
+        tennisBallWeight += 58
+    }
+  })
+  tennisBag['weight'] = tennisBallWeight
+  return tennisBag
 }
 
 console.log(ballCollector([58, 68, 62, 69, 58]))
