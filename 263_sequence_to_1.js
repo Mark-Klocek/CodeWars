5a5b95cc2ec1b0dadb12783256dea4d568afa021
Mark Klocek <Mark.Klocek@GMail.com>
2026-03-12 10:40:08 -0700

    codewars 263 exercise

---
+++ 263_sequence_to_1.js
@@ -0,0 +1,16 @@
+// Example : 
+// n=5  >> [5,4,3,2,1]
+// n=-1 >> [-1,0,1]
+
+// Range :
+// Python     -9999 < n < 9999
+// Javascript -9999 < n < 9999
+// c++        -9999 < n < 9999
+// Crystal    -9999 < n < 9999
+// Ruby       -9999 < n < 9999
+
+function seqToOne(n){
+  
+}
+
+console.log(seqToOne(10))
