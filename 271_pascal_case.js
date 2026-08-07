09cc6832b5898571b2d77a4e767e15087bdd0024
Mark Klocek <Mark.Klocek@GMail.com>
2026-08-06 18:31:53 -0700

    codewars 271 exercise

---
+++ 271_pascal_case.js
@@ -0,0 +1,22 @@
+// Description:
+
+// Write a method (or function, depending on the language) that converts a string to PascalCase, that is, all words must have their first letter capitalized and spaces must be removed.
+// Examples (input --> output):
+
+// "hello case" --> "HelloCase"
+// "pascal case word" --> "PascalCaseWord"
+
+// Don't forget to rate this kata! Thanks :)
+
+// Note: for historical reasons, the function is named camelCase() or similar in some languages, but it should actually perform conversion to PascalCase.
+
+//P : we are given a string of words
+
+//R : we are to return the string of given words in Pascal format
+
+//E : my name is mark => MyNameIsMark
+
+
+String.prototype.camelCase=function(){
+  //P:
+}
