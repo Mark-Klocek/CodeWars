36c09195f743e1ff84e5725085e11905c51491f7
Mark Klocek <Mark.Klocek@GMail.com>
2026-08-06 18:28:42 -0700

    codewars 270 exercise

---
+++ 270_char_ascii.js
@@ -0,0 +1,19 @@
+// Write a function which takes a number and returns the corresponding ASCII char for that value.
+
+// Example:
+
+// 65 --> 'A'
+// 97 --> 'a'
+// 48 --> '0
+
+//P We are given an integer
+
+//R We are asked to return the character corresponding to the ascii code of the integer we are given
+
+//E 65 => "A"
+
+
+function getChar(c){
+  //P use the fromcharcode method
+  
+}
