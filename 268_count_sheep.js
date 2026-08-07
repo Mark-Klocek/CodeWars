5c792320882e022fc1ef0de66e8600182394538b
Mark Klocek <Mark.Klocek@GMail.com>
2026-08-06 18:19:13 -0700

    268 codewars exercise

---
+++ 268_count_sheep.js
@@ -0,0 +1,27 @@
+// Consider an array/list of sheep where some sheep may be missing from their place. We need a function that counts the number of sheep present in the array (true means present).
+
+// For example,
+
+// [true,  true,  true,  false,
+//   true,  true,  true,  true ,
+//   true,  false, true,  false,
+//   true,  false, false, true ,
+//   true,  true,  true,  true ,
+//   false, false, true,  true]
+
+// The correct answer would be 17.
+
+// Hint: Don't forget to check for bad values like null/undefined
+
+//P We are given an array with values containing several things, but the one we are looking for is true
+
+//R: We are returning the amount of "sheep that are in place" (basically means true)
+
+//E: we are given ["true","false","undefined"] => 1
+
+
+function countSheeps(sheep) {
+  // iterate over the array, if the current element is true, we add it to return value
+  // return value of total amount of true in the array
+    
+}
