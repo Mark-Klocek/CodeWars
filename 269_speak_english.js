1a41999b87400ed46056d2adb3fc6d12170d2b9e
Mark Klocek <Mark.Klocek@GMail.com>
2026-08-06 18:23:01 -0700

    codewars 269 exercise

---
+++ 269_speak_english.js
@@ -0,0 +1,19 @@
+// Given a string of arbitrary length with any ascii characters. Write a function to determine whether the string contains the whole word "English".
+
+// The order of characters is important -- a string "abcEnglishdef" is correct but "abcnEglishsef" is not correct.
+
+// Upper or lower case letter does not matter -- "eNglisH" is also correct.
+
+// Return value as boolean values, true for the string to contains "English", false for it does not.
+
+//P: we are given a string of ascii characters
+
+//R: we are asked to return a boolean displaying whether or not the word english is in the string
+
+//E: wowoenglish => true
+
+
+
+function spEng(sentence){
+//P:
+}
