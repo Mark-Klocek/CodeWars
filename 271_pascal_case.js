6685cce4343ef793f2d8567ddc40d225e99748fd
Mark Klocek <Mark.Klocek@GMail.com>
2026-08-06 18:45:37 -0700

    codewars 271 solution

--- 271_pascal_case.js
+++ 271_pascal_case.js
@@ -12,11 +12,33 @@
 
 //P : we are given a string of words
 
-//R : we are to return the string of given words in Pascal format
+//R : we are to return the string of given words in Pascal format. What if there are capital letters in the midle of each word? 
 
 //E : my name is mark => MyNameIsMark
 
+let myString = "thIs is My stRing"
+function camelCase(string){
 
-String.prototype.camelCase=function(){
-  //P:
+    //P: make sure all letters are uniform from the initial string input
+    //  split each word in the original sentence by space, so we have each word separated
+    //  make the first letter of each word capitalized
+    //  rejoin the array of words into a single string, making it pascal formatted
+    string = string.toLowerCase()
+    let returnString = ''
+    let wordArray = string.split(' ')
+    wordArray.map(e=> {
+        let firstLetter = e[0].toUpperCase()
+        let endOfWord = e.slice(1)
+        let entireWord = firstLetter+endOfWord
+        returnString += entireWord
+    })
+    
+    
+
+
+
+
+  return returnString
 }
+
+console.log(camelCase(myString))
