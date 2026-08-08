0926c61aa21fb8a1e8daadf5307e4bb1771f2227
Mark Klocek <Mark.Klocek@GMail.com>
2026-08-07 17:21:53 -0700

    codewars 273 solution

--- 273_operations.js
+++ 273_operations.js
@@ -12,6 +12,17 @@
 
 function basicOp(operation, value1, value2){
   
-  
+  switch(operation){
+    case "+":
+        return value1 + value2
+    case "-":
+        return value1 - value2
+    case "/":
+        return value1 / value2
+    case "*":
+        return value1 * value2
+    default:
+        return "oops"
+  }
   return 0;
 }
