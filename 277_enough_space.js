2e4665fd9c391ea7aa5c368d72afc2f9784b38a7
Mark Klocek <Mark.Klocek@GMail.com>
2026-08-08 20:23:45 -0700

    codewars 277 solution

--- 277_enough_space.js
+++ 277_enough_space.js
@@ -12,4 +12,5 @@
 // If there is enough space, return 0, and if there isn't, return the number of passengers he can't take.
 
 function enough(cap, on, wait) {
-  
+  return cap - on >= wait ? 0 : wait - (cap - on)
+}
